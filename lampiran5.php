7d7e976a039903382f7bc3b77e7a70b723ecb977
shintiasari <32458115+shintiasari@users.noreply.github.com>
2018-01-21 17:22:58 +0800

    Menyelesaikan tugas todolist lampiran 5

---
+++ lampiran5.php
@@ -0,0 +1,390 @@
+<?php
+include ('head.php');
+?>
+
+<style type="text/css" scoped>
+table.GeneratedTable {
+width:100%;
+background-color:#FFFFFF;
+border-collapse:collapse;border-width:1px;
+border-color:#336600;
+border-style:inset;
+color:#000000;
+}
+
+table.GeneratedTable td, table.GeneratedTable th {
+border-width:1px;
+border-color:#000000;
+border-style:inset;
+padding:2px;
+}
+
+table.GeneratedTable thead {
+background-color:#FFFFFF;
+}
+</style>
+
+<style type="text/css">
+		.utama {
+			margin: auto;
+			border: 1px solid #FFFFFF;
+			padding: 13px;
+		}
+		.header {
+			font-size: 18px;
+			text-align: center;
+			background-color: #EDF4F6;
+
+
+		}
+		.table1 {
+			font-size: 15px;
+			font-family: arial;
+		}	
+		.table2 tr td {
+			border-right: 1px solid #FFFFFF;
+			border-left:1px solid #FFFFFF;
+			font-size: 14px;
+			font-family: arial;
+			text-align: center;
+		}
+		.ttd {
+			width:300px; 
+			float: right;
+			margin-top: -10px;
+			text-align: center;
+			font-weight: bold;
+		}
+	</style>
+
+	</head>
+<body>
+<div class="col-sm-12">
+	<div class="utama">
+		<p class="header">
+			
+			<table align="right">
+				<tr><td>LAMPIRAN V KEPUTUSAN BERSAMA MENTERI</td></tr>
+				<tr><td>PENDIDIKAN DAN KEBUDAYAAN DAN</td></tr>
+				<tr><td>KEPALA BADAN KEPEGAWAIAN NEGARA</td></tr>
+				<tr><td>NOMOR</td><td width="20px"><center>:</center></td><td> 61409/MPK/KP/99</td></tr>
+				<tr><td>NOMOR</td><td><center>:</center></td><td> 181 TAHUN 1999</td></tr>
+				<tr><td>TANGGAL</td><td><center>:</center></td><td> 13 OKTOBER 1999</td></tr>
+			</table>
+</p>
+
+<h1 style="font-size:19px; text-align:center"><b>SURAT PERNYATAAN</b><br>
+		<b>MELAKSANAKAN KEGIATAN PENUNJANG TRI DHARMA PERGURUAN TINGGI</b></h1>
+		<table border="0" class="table1">
+			<tr>
+				<td colspan="2" style="padding-top:20px; padding-bottom:20px">Yang bertanda tangan dibawah ini</td>
+				<td width="10px">:</td>
+			</tr>
+			<tr>
+				<td rowspan="5" width="15px"></td>
+				<td>Nama </td>
+				<td>:</td>
+				<td><b>SUTRISNO, S.E, M.M</td>
+			</tr>
+			<tr>
+				<td>NIP / NIK</td>
+				<td>:</td>
+				<td>19670512 200501 1 001</td>
+			</tr>
+			<tr>
+				<td>Pangkat / Golongan Ruang </td>
+				<td>:</td>
+				<td>Penata, III/c</td>
+			</tr>
+			<tr>
+				<td>Jabatan Fungsional </td>
+				<td>:</td>
+				<td>Lektor (300)</td>
+			</tr>
+			<tr>
+				<td>Unit Kerja </td>
+				<td>:</td>
+				<td>STIE Panca Setia Banjarmasin</td>
+			</tr>
+			<tr
+				><td colspan="2" style="padding-top:20px; padding-bottom:20px">Yang bertanda tangan dibawah ini </td>
+				<td width="10px">:</td>
+			</tr>
+			<tr>
+				<td rowspan="5"></td>
+				<td>Nama </td>
+				<td>:</td>
+				<td><b>ARIEF NOVIARAKHMAN ZAGLADI, SE, MM</td>
+			</tr>
+			<tr>
+				<td>NIP / NIK</td>
+				<td>:</td>
+				<td>1107118301</td>
+			</tr>
+			<tr>
+				<td>Pangkat / Golongan Ruang </td>
+				<td>:</td>
+				<td>-</td>
+			</tr>
+			<tr>
+				<td>Jabatan Fungsional </td>
+				<td>:</td>
+				<td>Asisten Ahli (100), 1 September 2007</td>
+			</tr>
+			<tr>
+				<td>Unit Kerja </td>
+				<td>:</td>
+				<td>STIE Panca Setia Banjarmasin</td>
+			</tr>
+			<tr>
+				<td colspan="4"><p align="left"><br> Telah melakukan kegiatan Penunjang Tri Dharma Perguruan Tinggi sebagai berikut :</p></td></tr>
+		</table>
+
+
+<div class="col-sm-12">			
+
+</div>
+<div class="col-sm-12">
+<table class="GeneratedTable">
+<thead>
+<tr>
+<th>NO.</th>
+<th><p><center>KEGIATAN PENUNJANG</center></p>				
+<p><center>TRI DHARMA PERGURUAN TINGGI</center></p>				
+	</th>
+<th><p><center>KEDUDUKAN/</center></p>
+<p><center>TINGKAT</center></p>		
+	</th>
+<th><p><center>TEMPAT/</center></p>
+<p><center>INSTANSI</center></p>
+</th>
+<th><p><center>TANGGAL</center></p>
+</th>
+<th><p><center>JML. ANGKA</center></p>
+<p><center>KREDIT</center></p></th>
+<th><p><center>KETERANGAN/</center></p>
+<p><center>BUKTI FISIK</center></p></th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td><center><b>1</b></center></td>
+<td><center><b>2</b></center></td>
+<td><center><b>3</b></center></td>
+<td><center><b>4</b></center></td>
+<td><center><b>5</b></center></td>
+<td><center><b>6</b></center></td>
+<td><center><b>7</b></center></td>
+</tr>
+<tr>
+<td></td>
+<td><b><u>Menjadi Panitia Dalam Suatu Badan/				
+Perguruan Tinggi</u></b></td>
+
+</tr>
+<tr>
+<td><center>1</center></td>
+<td>Menjadi Panitia Tim Kerja Penyusun
+Borang Akreditasi Program Studi 				
+Manajemen 				
+</td>
+<td><center>Ketua</center></td>
+<td><center>STIE Panca	
+Setia B.Masin</center></td>
+<td><center>13 Juli	
+2010</center>	
+</td>
+<td><center><b>2</b></center></td>
+<td>SK Nomor :		
+010/PTS 240.XI.01/Kep/		
+Q 2010		
+</td>
+</tr>
+
+<tr>
+<td><center>2</center></td>
+<td>Menjadi Panitia Tim Kerja Penyusun				
+Borang Akreditasi Institusi				
+</td>
+<td><center>Anggota</center></td>
+<td><center>STIE Panca	
+Setia B.Masin</center></td>
+<td>13 Juli	
+2010	
+</td>
+<td><center><b>1</b></center></td>
+<td>SK Nomor :		
+009/PTS 240.XI.01/Kep/		
+Q 2010		
+</tr>
+
+<tr>
+<td><center>3</center></td>
+<td>Menjadi Panitia Dalam Rangka Pengu-				
+kuhan Dr.H.Musthafa, SE, MM Dalam 				
+Jabatan Akademik Guru Besar 				
+</td>
+<td><center>Anggota</center></td>
+<td></td>
+<td><center>16 Agustus	
+2010</center></td>
+<td><center><b>1</b></center></td>
+<td>Surat Penunjukan No.:		
+2628/PTS 240.XI.01/U		
+2010</td>
+</tr>
+
+<tr>
+<td><center>4</center></td>
+<td>Menjadi Panitia Tim Kerja Penyusun				
+Evaluasi Diri Program Studi Manaje-				
+men				
+</td>
+<td><center>Ketua</center></td>
+<td><center>STIE Panca	
+Setia B.Masin</center></td>
+<td><center>4 Agustus
+2010</center>	
+</td>
+<td><center><b>2</b></center></td>
+<td>SK Nomor :		
+011/PTS 240.XI.01/Kep/		
+Q 2010			
+</tr>
+
+<tr>
+<td><center>5</center></td>
+<td>Menjadi Panitia Dalam Rangka Pelaksa-				
+naan Workshop Metodologi Penelitian				
+Dan Seminar "Peningkatan Prestasi 				
+Kerja Dengan Feng Shui dan Perilaku							
+</td>
+<td><center>Anggota</center></td>
+<td><center>STIE Panca	
+Setia B.Masin</center></td>
+<td><center>20 Oktober	
+2010</center>		
+</td>
+<td><center><b>1</b></center></td>
+<td>Surat Penunjukan No.:		
+3297/PTS 240.XI.01/U		
+2010				
+</tr>
+
+<tr>
+<td></td>
+<td><b><u>Berperan Serta Aktif Dalam Pertemuan				
+Ilmiah</u></b></td>
+</tr>
+
+<tr>
+<td><center>1</center></td>
+<td>Mengikuti Lokakarya Nasional Penyusu -				
+nan Proposal Penelitian Bidang Ekono -				
+mi Untuk Hibah Dari Dikti								
+</td>
+<td><center>Peserta</center></td>
+<td><center>ISEI Cabang	
+Jawa Timur</center></td>
+<td><center>4-5 Maret 	
+2009</center>			
+</td>
+<td><center><b>1</b></center></td>
+<td>Sertifikat					
+</tr>
+
+
+<tr>
+<td><center>2</center></td>
+<td>Mengikuti Seminar Sosialisasi dan Work				
+Shop Direktorat Sistem Informasi, Pe -				
+rangkat Lunak Dan Konten												
+</td>
+<td><center>Peserta</center></td>
+<td><center>Departemen	
+Komunikasi	
+& Informasi</center></td>
+<td><center>17-18 Maret	
+2010</center>				
+</td>
+<td><center><b>1</b></center></td>
+<td>Sertifikat					
+</tr>
+
+<tr>
+<td><center><b>1</b></center></td>
+<td><center><b>2</b></center></td>
+<td><center><b>3</b></center></td>
+<td><center><b>4</b></center></td>
+<td><center><b>5</b></center></td>
+<td><center><b>6</b></center></td>
+<td><center><b>7</b></center></td>
+</tr>
+
+<tr>
+<td><center>3</center></td>
+<td>Mengikuti Seminar Sosialisasi dan Work				
+Shop Direktorat Sistem Informasi, Pe -				
+rangkat Lunak Dan Konten												
+</td>
+<td><center>Peserta</center></td>
+<td><center>PT Bank	
+Mandiri</center></td>
+<td><center>22-23 Juli	
+2010</center>				
+</td>
+<td><center>1</center></td>
+<td>Sertifikat					
+</tr>
+
+<tr>
+<td><center>4</center></td>
+<td>Mengikuti Seminar QLA-Training Syner-				
+gy Building Mahardhika Group								
+</td>
+<td><center>Peserta</center></td>
+<td><center>RIDAM	
+Partner In	
+Change</center></td>
+<td><center>27-29 Juli	
+2009</center>					
+</td>
+<td><center>1</center></td>
+<td>Sertifikat Nomor : 		
+Ridam-101/PL-03/VII/		
+2009							
+</tr>		
+
+<tr>
+<td><center></center></td>
+<td><center><b>JUMLAH</b></center></td>
+<td><center></center></td>
+<td><center></center></td>
+<td><center></center></td>
+<td><center><b>11</b></center></td>
+<td><center></center></td>
+</tr>									
+
+
+
+</tbody>
+</table>
+
+</div>
+ 
+ <p class="table1">Demikian pernyataan ini dibuat untuk dapat dipergunakan sebagaimana mestinya.</p>
+		<div class="ttd">Banjarmasin, 30 April 2011<br><br>
+					Yang Membuat Pernyataan<br>
+					Puket I STIE Panca Setia B.Masin<br><br><br><br>				 
+					Sutrisno, SE, MM<br>
+					NIP. 19670512 200501 1 001
+		</div>
+		<br><br><br><br><br><br><br>
+		<b>catatan :</b> <br>
+		1. Dibuat per semester.<br>
+		2. Ditanda tangani oleh Ketua Jurusan pada Univ/Insititut/Sekolah Tinggi/Akademi/Politeknik.<br>
+		3. Dilampirkan bukti-bukti kegiatan tersebut di atas.
+</div>
+
+<img src="img/btn_print.png" width="35" onclick="window.print();">
+</html>
